5e8dd4035ff0df0480a867e3e7556c6d18e2ec05
Merlijn Boogerd <mboogerd@xebia.com>
2021-07-02 11:10:31 +0200

    Remove separation

--- chat/index.js
+++ chat/index.js
@@ -11,9 +11,7 @@ OrbitDB.createInstance(ipfs).then(async (orbitdb) => {
     // Setup write access
     accessController: {
       write: [
-        // Give access to ourselves
         "033eda95ba4c528875c692c4c7f267b51bc42807a289f1657c23594c37e5b2fcd6", // Merlijn
-        // Give access to the second peer
         "03990f6dabfd6d20a1cae72d22262089429659fdc885b8c45dc42572984b891fd8", // Leon
         "02d418b70b13c79773aa74998c26d26af828a157a90096c3e25ef7a31f69cb9055", // Arjan
         "03bd95a4b1d91681648bfb1a9f7554ce9d14c1cf6290d8804d685f20963c80d7cc", // Jethro
