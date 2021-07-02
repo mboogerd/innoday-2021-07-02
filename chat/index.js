4eeb1675d815fdacd003a2fc2089d59cd1bd410b
Merlijn Boogerd <mboogerd@xebia.com>
2021-07-02 13:24:29 +0200

    join and leave

--- chat/index.js
+++ chat/index.js
@@ -23,29 +23,68 @@ OrbitDB.createInstance(ipfs).then(async (orbitdb) => {
     },
   };
 
+  const channels = await orbitdb.keyvalue(
+    "/orbitdb/zdpuAuBYiKzBeebGAsfWLFBKonJuA5QzhFLfNr4Z6S12aWXkf/channels",
+    options
+  );
+
+  console.log(channels.address);
   // const spawnDB = await orbitdb.log(`chatting-together`, options);
   // console.log(spawnDB.address);
 
-  const db = await orbitdb.log(
-    `/orbitdb/zdpuAsyNpbZvcZK4Gwp8CNux5xWZyLkSdLA7qQUseMbzpvNVZ/chatting-together`,
-    options
-  );
-  await db.load();
-
-  db.events.on(
+  channels.events.on(
     "replicate",
     (address, entry) => entry.payload && console.log(entry.payload.value)
   );
 
-  db.events.on("log.op.ADD", (id, hash, payload) =>
-    console.log(`> ${payload?.value}`)
-  );
+  var activeChannel = undefined;
 
-  console.log(db.address);
+  // console.log(channels.address);
 
   const rl = readline.createInterface(process.stdin);
 
-  rl.on("line", (message) => {
-    db.add(`[${process.env["USER"]}]: ${message}`);
+  rl.on("line", async (message) => {
+    if (message.startsWith("/join")) {
+      var channel = getChannel(message);
+
+      activeChannel = await orbitdb.log(
+        `/orbitdb/zdpuAsyNpbZvcZK4Gwp8CNux5xWZyLkSdLA7qQUseMbzpvNVZ/chatting-together`,
+        options
+      );
+      await activeChannel.load();
+      join(activeChannel);
+    }
+
+    if (message.startsWith("/create")) {
+      var channel = getChannel(message);
+      activeChannel = await orbitdb.log(
+        `/orbitdb/zdpuAsyNpbZvcZK4Gwp8CNux5xWZyLkSdLA7qQUseMbzpvNVZ/chatting-together`,
+        options
+      );
+    }
+
+    if (message.startsWith("/leave")) {
+      leave(activeChannel);
+      activeChannel == undefined;
+    }
+
+    // db.add(`[${process.env["USER"]}]: ${message}`);
   });
 });
+
+const getChannel = (message) => message.split("-")[1];
+
+const join = (activeChannel) => {
+  activeChannel.events.on(
+    "replicate",
+    (address, entry) => entry.payload && console.log(entry.payload.value)
+  );
+
+  activeChannel.events.on("log.op.ADD", (id, hash, payload) =>
+    console.log(`> ${payload?.value}`)
+  );
+};
+
+const leave = (activeChannel) => {
+  activeChannel.close();
+};
