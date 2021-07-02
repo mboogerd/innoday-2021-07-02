98e23d3eda06d5d45264732046fb3285f360e8cd
Merlijn Boogerd <mboogerd@xebia.com>
2021-07-02 15:28:32 +0200

    Correct bug

--- chat/index.js
+++ chat/index.js
@@ -149,7 +149,7 @@ const leave = async () => {
   if (activeChannel) {
     activeCounter.inc(-1);
     systemLog(`Leaving ${activeChannel.address.path}`);
-    activeChannel.add(`${process.env["USER"]} left ${foundChannel.address}`);
+    activeChannel.add(`${process.env["USER"]} left ${activeChannel.address}`);
     await activeChannel.close();
     activeChannel = undefined;
     activeCounter = undefined;
